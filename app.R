b8d2d8bec6d732f1d861703d38de49df5ba5b8d0
grahamwuk <62952928+grahamwuk@users.noreply.github.com>
2020-04-13 18:14:26 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -4,7 +4,12 @@ library(tidyverse)
 library(shiny)
 library(scales)
 library(gapminder)
+library(plotly)
+library(RColorBrewer)
+
+#Working Directory
 #setwd("C:/Users/Graham/Documents/R/My Codes/Shiny/Coronavirus App")
+#setwd("C:/Users/lalisimus/Documents/Cool_Codes")
 
 #Data Import and Cleansing----
 
@@ -19,7 +24,10 @@ covid_case_raw=covid_case_raw[order(covid_case_raw$Entity,covid_case_raw$Date_fo
 covid_case_neat <- covid_case_raw %>%
   rename(country=Entity, code=Code, date=Date_formatted, cases = `Total.confirmed.cases.of.COVID.19..cases.`) %>%
   select(country,code,date,cases) %>%
-  filter(!country %in% c("World","Africa","Asia","Europe","South America","North America","Oceania","World excl. China","World excl. China and South Korea","World excl. China, South Korea, Japan and Singapore"))
+  filter(!country %in% c("World","Africa","Asia","Asia excl. China","Europe","European Union","South America","North America","Oceania","World excl. China","World excl. China and South Korea","World excl. China, South Korea, Japan and Singapore","High income","Low income","Upper middle income","Lower middle income"))
+country_list <- covid_case_neat %>%
+  select(country) %>%
+  distinct()
 
 #Deaths
 covid_death_raw=read.csv(file="www/total-deaths-covid-19.csv",stringsAsFactors = F)
@@ -32,7 +40,7 @@ covid_death_raw=covid_death_raw[order(covid_death_raw$Entity,covid_death_raw$Dat
 covid_death_neat <- covid_death_raw %>%
   rename(country=Entity, code=Code, date=Date_formatted, deaths = `Total.confirmed.deaths.due.to.COVID.19..deaths.`) %>%
   select(country,date,deaths) %>%
-  filter(!country %in% c("World","Africa","Asia","Europe","South America","North America","Oceania","World excl. China","World excl. China and South Korea","World excl. China, South Korea, Japan and Singapore"))
+  filter(!country %in% c("World","Africa","Asia","Asia excl. China","Europe","European Union","South America","North America","Oceania","World excl. China","World excl. China and South Korea","World excl. China, South Korea, Japan and Singapore","High income","Low income","Upper middle income","Lower middle income"))
 
 #Lockdowns
 lockdowns <- read_csv("www/lockdowns.csv") %>%
@@ -94,7 +102,7 @@ ui <- fluidPage(
   titlePanel("COVID-19 Data Explorer"),
   fluidRow(
     column(4,
-           sliderInput(inputId="num",label="Select How Many Countries To Include",value=10, min=1, max=200),
+           sliderInput(inputId="num",label="Select How Many Countries To Include",value=10, min=1, max=200)
     ),
     column(4,
            selectInput(inputId="ColorBy", 
@@ -104,56 +112,54 @@ ui <- fluidPage(
     ),
     column(4,
            checkboxInput(inputId="PerCap", 
-                       label="Make figures per 100k population", 
+                         label="Make figures per 100k population" 
            )
-    ),
+    )
   ),
   tabsetPanel(
-              tabPanel(title="Cases",
-                       mainPanel(plotOutput("cases_date")),
-                       sliderInput(inputId="case_threshold",label="Select Case Threshold for Alignment",value=100, min=1, max=1000),
-                       mainPanel(plotOutput("cases_align")),
-                       mainPanel(plotOutput("cases_bar"))),
-              tabPanel(title="Deaths",
-                       mainPanel(plotOutput("deaths_date")),
-                       sliderInput(inputId="death_threshold",label="Select Death Threshold for Alignment",value=1, min=1, max=100),
-                       mainPanel(plotOutput("deaths_align")),
-                       mainPanel(plotOutput("deaths_bar"))),
-              tabPanel(title="Deaths v Cases",
-                       mainPanel(plotOutput("dpc_scatter")),
-                       mainPanel(plotOutput("dpc_bar"))),
-              tabPanel(title="DataTable",
-                       "Latest Data: ", max(covid_joined$date),
-                       DT::dataTableOutput("tableFiles")
-                       )
-              )
+    tabPanel(title="Cases",
+             mainPanel(plotlyOutput("cases_date")),
+             sliderInput(inputId="case_threshold",label="Select Case Threshold for Alignment",value=100, min=1, max=1000),
+             mainPanel(plotlyOutput("cases_align")),
+             mainPanel(plotlyOutput("cases_bar"))),
+    tabPanel(title="Deaths",
+             mainPanel(plotlyOutput("deaths_date")),
+             sliderInput(inputId="death_threshold",label="Select Death Threshold for Alignment",value=1, min=1, max=100),
+             mainPanel(plotlyOutput("deaths_align")),
+             mainPanel(plotlyOutput("deaths_bar"))),
+    tabPanel(title="Deaths v Cases",
+             mainPanel(plotlyOutput("dpc_scatter")),
+             mainPanel(plotlyOutput("dpc_bar"))),
+    tabPanel(title="DataTable",
+             "Latest Data: ", max(covid_joined$date),
+             DT::dataTableOutput("tableFiles")
+    )
+  )
 )
 
 #Server Function----
 server <- function(input, output) {
-
   #Run the dias functions on cases and deaths datasets, using selected input thresholds
   covid_calc <- reactive({
     a <- covid_joined %>%
       mutate(days_since_case_threshold=dias_cases(input$case_threshold,covid_joined)) %>%
       mutate(days_since_death_threshold=dias_deaths(input$death_threshold,covid_joined))
   })
-
   #Define the yaxis column depending on the selected input
-  # selected_yaxis <- reactive({
-  #   m <- covid_calc()
-  #   if (input$PerCap = TRUE) {m <- m %>% mutate(case_yaxis=cases_per_100kpop)}
-  #   if (input$PerCap = TRUE) {m <- m %>% mutate(case_yaxis=deaths_per_100kpop)}
-  #   if (input$PerCap = FALSE) {m <- m %>% mutate(case_yaxis=cases)}
-  #   if (input$PerCap = FALSE) {m <- m %>% mutate(case_yaxis=deaths)}
-  # })
+   selected_yaxis <- reactive({
+     m <- covid_calc()
+     if (input$PerCap == TRUE) {m <- m %>% mutate(case_yaxis=cases_per_100k_pop)}
+     if (input$PerCap == TRUE) {m <- m %>% mutate(death_yaxis=deaths_per_100k_pop)}
+     if (input$PerCap == FALSE) {m <- m %>% mutate(case_yaxis=cases)}
+     if (input$PerCap == FALSE) {m <- m %>% mutate(death_yaxis=deaths)}
+   })
   
   #Create filtered versions of both datasets only containing the countries of interest
   selected_countries <- reactive({
     g <- covid_joined_latest %>%
       top_n(input$num, wt=cases) %>%
       select(country) %>%
-      left_join(covid_calc(), by="country") %>%
+      left_join(selected_yaxis(), by="country") %>%
       arrange(desc(cases))
   })
   selected_countries_latest <- reactive({
@@ -161,53 +167,86 @@ server <- function(input, output) {
       top_n(input$num, wt=cases) %>%
       arrange(desc(cases))
   })
-
-  #Create all plots using ggplot
-  output$cases_date <- renderPlot({
-    ggplot(data = selected_countries(), 
-         mapping = aes(x = date, y = cases, color=country)) +
-    geom_line() +
-      scale_y_log10(labels=comma)
+  
+  #Create all plots using plotly
+  output$cases_date <- renderPlotly({
+    datos<-selected_countries()
+    datos<-datos[order(datos$country,datos$date),]
+    p<-plot_ly(datos,x=~date,y=~case_yaxis,name=~country,type="scatter",mode="lines")%>%
+      config(displayModeBar=T)%>%
+      layout(yaxis=list(type="log"))
+    p
   })
-  output$cases_align <- renderPlot({
-    ggplot(data = selected_countries(), 
-         mapping = aes(x = days_since_case_threshold, y = cases, color=country)) +
-    geom_line() +
-      scale_y_log10(labels=comma,limits=c(input$case_threshold,NA))
+  output$cases_align <- renderPlotly({
+    datos<-selected_countries()
+    datos<-datos[datos$cases>=as.numeric(input$case_threshold),]
+    datos<-datos[order(datos$country,datos$days_since_case_threshold),]
+    p<-plot_ly(datos,x=~days_since_case_threshold,y=~case_yaxis,name=~country,type="scatter",mode="lines")%>%
+      config(displayModeBar=T)%>%
+      layout(yaxis=list(type="log"))
+    p
   })
-  output$deaths_date <- renderPlot({
-    ggplot(data = selected_countries(), 
-           mapping = aes(x = date, y = deaths, color=country)) +
-      geom_line() +
-      scale_y_log10(labels=comma)
+  output$deaths_date <- renderPlotly({
+    datos<-selected_countries()
+    datos<-datos[order(datos$country,datos$date),]
+    p<-plot_ly(datos,x=~date,y=~death_yaxis,name=~country,type="scatter",mode="lines")%>%
+      config(displayModeBar=T)%>%
+      layout(yaxis=list(type="log"))
+    p
   })
-  output$deaths_align <- renderPlot({
-    ggplot(data = selected_countries(), 
-           mapping = aes(x = days_since_death_threshold, y = deaths, color=country)) +
-      geom_line() +
-      scale_y_log10(labels=comma,limits=c(input$death_threshold,NA))
+  output$deaths_align <- renderPlotly({
+    datos<-selected_countries()
+    datos<-datos[order(datos$country,datos$days_since_death_threshold),]
+    p<-plot_ly(datos,x=~days_since_death_threshold,y=~death_yaxis,name=~country,type="scatter",mode="lines")%>%
+      config(displayModeBar=T)%>%
+      layout(yaxis=list(type="log"))
+    p
   })
-  output$dpc_scatter <- renderPlot({
-    ggplot(data = covid_joined_latest, 
-           mapping = aes(x = cases, y = deaths, color=1/deaths_per_case)) +
-      geom_point() +
-      scale_x_log10(labels=comma) +
-      scale_y_log10(labels=comma)
+  output$dpc_scatter <- renderPlotly({
+    datos<-covid_joined_latest
+    datos<-datos[datos$deaths_per_case>0,]
+    datos<-datos[order(datos$deaths),]
+    p<-plot_ly(datos,x=~cases,y=~deaths,type="scatter",mode="markers",color=~1/deaths_per_case
+    )%>%
+      config(displayModeBar=T)%>%
+      layout(yaxis=list(type="log"),xaxis=list(type="log"))
+    p
   })
-  output$cases_bar <- renderPlot({
-    ggplot(data = selected_countries_latest(), 
-           mapping = aes(x = country, y = cases)) +
-      geom_col()
+  output$cases_bar <- renderPlotly({
+    datos<-selected_countries_latest()
+    datos$country <- factor(datos$country, levels = unique(datos$country)[order(datos$cases, decreasing = TRUE)])
+    datos<-datos[order(datos$country),]
+    p<-plot_ly(datos,x=~country,y=~cases,type="bar")%>%
+      config(displayModeBar=T) %>%
+      layout(
+        xaxis = list(title = "",
+                     categoryorder = "array",
+                     categoryarray = ~cases))
+    p
   })
-  output$deaths_bar <- renderPlot({
-    ggplot(data = selected_countries_latest(), 
-           mapping = aes(x = country, y = deaths)) +
-      geom_col()
+  output$deaths_bar <- renderPlotly({
+    datos<-selected_countries_latest()
+    datos$country <- factor(datos$country, levels = unique(datos$country)[order(datos$deaths, decreasing = TRUE)])
+    datos<-datos[order(datos$country),]
+    p<-plot_ly(datos,x=~country,y=~deaths,type="bar")%>%
+      config(displayModeBar=T) %>%
+      layout(
+        xaxis = list(title = "",
+                     categoryorder = "array",
+                     categoryarray = ~deaths))
+    p
   })
-  output$dpc_bar <- renderPlot({
-    ggplot(data = selected_countries_latest(), 
-           mapping = aes(x = country, y = deaths_per_case)) +
-      geom_col()
+  output$dpc_bar <- renderPlotly({
+    datos<-selected_countries_latest()
+    datos$country <- factor(datos$country, levels = unique(datos$country)[order(datos$deaths_per_case, decreasing = TRUE)])
+    datos<-datos[order(datos$country),]
+    p<-plot_ly(datos,x=~country,y=~deaths_per_case,type="bar")%>%
+      config(displayModeBar=T) %>%
+      layout(
+        xaxis = list(title = "",
+                     categoryorder = "array",
+                     categoryarray = ~deaths_per_case))
+    p
   })
   output$tableFiles <- DT::renderDataTable(covid_joined_latest)
 }
